e94c4c56f00eff7337818c92dc30262d5351e5f3
nawedume <umar.nawed@mail.utoronto.ca>
2019-03-20 13:06:39 -0400

    Added bitmap to mif converter

---
+++ src/tools/bitconverter.py
@@ -0,0 +1,30 @@
+from scipy import misc
+import sys
+
+def convert(image_array):
+    counter = 0
+
+    print('''
+    DEPTH = 4092; 
+    WIDTH = 8;
+    ADDRESS_RADIX = HEX;
+    DATA_RADIX = HEX;
+
+    CONTENT BEGIN
+    ''')
+
+    for row in image_array:
+        for pixel in row:
+            for color in pixel:
+                print("     %03x : %02x" % (counter, color))
+                counter += 1
+
+    print('''END;''')
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        sys.stderr.write("usage: %s image" % sys.argv[0])
+        exit(1)
+    
+    image_array = misc.imread(sys.argv[1], mode='RGB')
+    convert(image_array)
